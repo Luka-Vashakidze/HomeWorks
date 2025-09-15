91f03b3723949092b87ca26def7286c1d40d6e8f
Luka Vashakidze <lukavashakidze15@gmail.com>
2025-09-16 03:54:11 +0400

    add homework2

---
+++ Homework2/Homework2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Homework2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.WriteLine("Luka Vashakidze");
+
+            Console.Write("enter your input ");
+            string input = Console.ReadLine();
+
+            Console.WriteLine("your input is: " + input);
+
+        }
+    }
+}
